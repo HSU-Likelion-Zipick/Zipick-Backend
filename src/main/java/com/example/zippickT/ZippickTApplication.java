050ac3a0731095d10e6f314f951e27279688563c
naeunmin <babaleo115@gmail.com>
2025-05-15 22:59:42 +0900

    스프링 프로젝트 생성 및 기본 설정

---
+++ src/main/java/com/example/zippickT/ZippickTApplication.java
@@ -0,0 +1,13 @@
+package com.example.zippickT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZippickTApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZippickTApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/zippickT/ZippickTApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zippickT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZippickTApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
