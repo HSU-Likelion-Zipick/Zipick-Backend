18db0d482050334ef25ff435ee6e9db96b98b06e
LgE02 <leegaeun023@gmail.com>
2025-05-17 20:47:38 +0900

    :sparkles: feat : swagger 의존성, 기본 설정 추가

---
+++ src/main/java/com/example/zippickT/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.example.zippickT.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .addServersItem(new Server().url("/api"))
+                /// [현재 Swagger UI가 열려 있는 주소]
+                /// = 도메인 + 포트
+                /// +
+                /// .addServersItem(new Server().url("/api"))
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Zipick API") // API의 제목
+                .description("Zipick 프로젝트의 API 테스트") // API에 대한 설명
+                .version("1.0.0"); // API의 버전
+    }
+}
