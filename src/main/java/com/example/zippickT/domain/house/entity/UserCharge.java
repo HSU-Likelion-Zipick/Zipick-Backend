c0d1c6228d1029584ca5143e51c9a60755539189
naeunmin <babaleo115@gmail.com>
2025-05-20 11:44:09 +0900

    Feat : UserCharge Entity 생성

---
+++ src/main/java/com/example/zippickT/domain/house/entity/UserCharge.java
@@ -0,0 +1,24 @@
+package com.example.zippickT.domain.house.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserCharge {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userHouuseInfo_id")
+    private UserHouseInfo userHouseInfo;
+
+}
