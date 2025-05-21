739443e668f8909043148c5751fdf9b6d038d0b9
LgE02 <leegaeun023@gmail.com>
2025-05-21 19:37:06 +0900

    :sparkles:Feat : 사용자 생성 요청 dto 코드 작성

--- src/main/java/com/example/zippickT/domain/User/service/UserServiceImpl.java
+++ src/main/java/com/example/zippickT/domain/User/service/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package com.example.zippickT.domain.User.service;
 
 import com.example.zippickT.domain.User.repository.UserRepository;
+import com.example.zippickT.domain.User.web.dto.CreateUserReq;
+import com.example.zippickT.domain.User.web.dto.CreateUserRes;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,4 +12,9 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+
+    @Override
+    public CreateUserRes createUser(CreateUserReq createUserReq) {
+        return null;
+    }
 }
