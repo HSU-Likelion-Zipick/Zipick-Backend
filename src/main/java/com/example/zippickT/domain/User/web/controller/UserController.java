9362b7b5521a3193a0a0057ce6e532e5f542218f
LgE02 <leegaeun023@gmail.com>
2025-05-22 00:16:42 +0900

    :sparkles:Feat : 사용자 정보 입력 API

--- src/main/java/com/example/zippickT/domain/User/web/controller/UserController.java
+++ src/main/java/com/example/zippickT/domain/User/web/controller/UserController.java
@@ -9,10 +9,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,7 +20,7 @@ public class UserController {
     private final UserService userService;
 
     //사용자 정보 생성
-    @GetMapping("/MyData")
+    @PostMapping("/MyData")
     public ResponseEntity<SuccessResponse<?>> createUserDate(
             @RequestBody @Valid CreateUserReq createUserReq
     ){
