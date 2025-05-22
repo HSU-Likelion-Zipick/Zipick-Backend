61c0358da3ff60d4249066fbe6392e00cf49b975
LgE02 <leegaeun023@gmail.com>
2025-05-22 11:18:14 +0900

    :sparkles:Feat : 유저 예외코드 추가

--- src/main/java/com/example/zippickT/domain/User/exception/UserErrorCode.java
+++ src/main/java/com/example/zippickT/domain/User/exception/UserErrorCode.java
@@ -10,8 +10,8 @@ public enum UserErrorCode implements BaseResponseCode {
     TRANSPORT_NOT_FOUND_404("TRANSPORT_NOT_FOUND_404",404,"유효하지 않는 이동수단입니다." ),
     GENDER_NOT_FOUND_404("GENDER_NOT_FOUND_404", 404, "유효하지 않는 성별입니다."),
     JOB_NOT_FOUND_404("JOB_NOT_FOUND_404", 404, "유효하지 않는 직업입니다." ),
-    LIFESTYLE_NOT_FOUND_404("LIFESTYLE_NOT_FOUND_404", 404, "유효하지 않는 생활패턴입니다.");
-
+    LIFESTYLE_NOT_FOUND_404("LIFESTYLE_NOT_FOUND_404", 404, "유효하지 않는 생활패턴입니다."),
+    USER_NOT_FOUND_404("USER_NOT_FOUND_404",404,"유저를 찾을 수 없습니다.");
 
     private final String code;
     private final int httpStatus;
