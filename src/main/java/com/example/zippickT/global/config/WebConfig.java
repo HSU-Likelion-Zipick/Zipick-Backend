785f36139cf563736cbbf5b6d4079a3a55d29e32
naeunmin <babaleo115@gmail.com>
2025-05-29 11:08:12 +0900

    Refactor : 웹 설정 코드 변경

--- src/main/java/com/example/zippickT/global/config/WebConfig.java
+++ src/main/java/com/example/zippickT/global/config/WebConfig.java
@@ -8,10 +8,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/*")
+        registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000","https://zipick.netlify.app") // 실제 프론트 주소 명시
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("")
+                .allowedHeaders("*")
                 .allowCredentials(true);
     }
 }
